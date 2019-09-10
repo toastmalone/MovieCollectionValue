af4ed2f06bfb7b7e85a769133fd06bfefc24f52f
toastmalone <josh.m.gauntt@gmail.com>
2019-09-10 10:09:45 -0700

    initial commit

---
+++ MovieCollectionValue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using IronWebScraper;
+
+namespace MovieCollectionValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderLocation;
+
+            folderLocation = Console.ReadLine();
+
+        }
+    }
+
+    class AmazonScraper : WebScraper
+    {
+        public override void Init()
+        {
+            this.LoggingLevel = WebScraper.LogLevel.All;
+            this.Request("amazon.com", Parse);
+        }
+
+        public override void Parse(Response response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
