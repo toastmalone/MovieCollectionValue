49bf0651aa72f1e11eafd03e23116e55ab7937b7
toastmalone <josh.m.gauntt@gmail.com>
2019-09-10 15:28:09 -0700

    made some progress

--- MovieCollectionValue/Program.cs
+++ MovieCollectionValue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using IronWebScraper;
 
@@ -8,24 +9,54 @@ namespace MovieCollectionValue
     {
         static void Main(string[] args)
         {
-            string folderLocation;
+            string moviesFolderLocation;
+            string[] movies;
+            do
+            {
+                Console.WriteLine("Input Location of Movie Folder");
+                moviesFolderLocation = Console.ReadLine();
+            } while (Directory.Exists(moviesFolderLocation) == false);
 
-            folderLocation = Console.ReadLine();
-
+            movies = Directory.GetDirectories(moviesFolderLocation);
+            for(int i = 0; i < movies.Length; i++)
+            {
+                movies[i] = movies[i].Replace(moviesFolderLocation + "\\", "");
+                movies[i] = movies[i].Replace(" ", "+");
+            }
+                AmazonScraper scraper = new AmazonScraper(movies);
+                scraper.Start();
         }
+
     }
 
     class AmazonScraper : WebScraper
     {
+        double value = 0.0;
+        List<string> _movies = new List<string>();
         public override void Init()
         {
             this.LoggingLevel = WebScraper.LogLevel.All;
-            this.Request("amazon.com", Parse);
+            this.Request(_movies, Parse);
         }
 
         public override void Parse(Response response)
         {
-            throw new NotImplementedException();
+            HtmlNode[] nodes = response.Css("#search  div > div:nth-child(2) > div:nth-child(1) > div > div > div.a-row.a-size-base.a-color-secondary > div > a > span");
+            foreach (HtmlNode node in nodes)
+            {
+                Console.WriteLine(node.ChildNodes[0].TextContent);
+            }
+        }
+
+        public AmazonScraper(string[] movies)
+        {
+            
+          for(int i = 0; i < movies.Length; i++)
+          {
+                _movies.Add($@"https://www.amazon.com/s?k={movies[i]}&i=instant-video&ref=nb_sb_noss_2");
+          }
+
+           
         }
     }
 }
