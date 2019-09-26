f368e4f5d6a81bdb82ff924f47a5389d9421445a
toastmalone <josh.m.gauntt@gmail.com>
2019-09-25 17:26:27 -0700

    Now searches for video extension files and can filter based on file size

--- MovieCollectionValue/Program.cs
+++ MovieCollectionValue/Program.cs
@@ -11,25 +11,58 @@ namespace MovieCollectionValue
         static void Main(string[] args)
         {
             string moviesFolderLocation;
-            string[] movies;
+            
+
+            List<FileInfo> movies = new List<FileInfo>();
             do
             {
                 Console.WriteLine("Input Location of Movie Folder");
                 moviesFolderLocation = Console.ReadLine();
             } while (Directory.Exists(moviesFolderLocation) == false);
 
-            movies = Directory.GetDirectories(moviesFolderLocation);
-            for(int i = 0; i < movies.Length; i++)
+            List<string> videoFilePaths = GetVideoFiles(moviesFolderLocation, "*.mp4|*.mkv|*.wav", SearchOption.AllDirectories);
+            List<string> videoFileNames = new List<string>();
+            
+            //remove small files
+            foreach (string vf in videoFilePaths)
             {
-                movies[i] = movies[i].Replace(moviesFolderLocation + "\\", "");
-                movies[i] = movies[i].Replace(" ", "+");
-                movies[i] = $@"https://www.amazon.com/s?k={movies[i]}&i=instant-video&ref=nb_sb_noss_2";
+                var fi = new FileInfo(vf);
+              /*  if (fi.Length > 200000000) */ videoFileNames.Add(fi.Name);
+                
             }
+            
+            for(int i = 0; i < videoFileNames.Count; i++)
+            {
+                videoFileNames[i] = videoFileNames[i].Replace("_", "+");
+                videoFileNames[i] = videoFileNames[i].Replace(" ", "+");
+                videoFileNames[i] = $@"https://www.amazon.com/s?k={videoFileNames[i]}&i=instant-video&ref=nb_sb_noss_2";
+            }
+            
 
-            AmazonScraper scraper = new AmazonScraper(movies);
+            AmazonScraper scraper = new AmazonScraper(videoFileNames.ToArray());
             scraper.Start();
             Console.WriteLine(scraper.collectionValue);
             Console.ReadLine();
+
+            List<string> GetVideoFiles(string directory, string searchPattern, SearchOption searchOption)
+            {
+                DirectoryInfo dir = new DirectoryInfo(directory);
+
+                string[] searchPatterns = searchPattern.Split("|");
+
+                List<string> allVideoFiles = new List<string>();
+
+                foreach(string sp in searchPatterns)
+                {
+                    allVideoFiles.AddRange(Directory.GetFiles(directory, sp, searchOption));
+                    for(int i=0; i<allVideoFiles.Count;i++)
+                    {
+                        allVideoFiles[i] = allVideoFiles[i].Replace(sp.Replace("*", ""), "");
+                    }
+                }
+
+                return allVideoFiles;
+            }
         }
 
     }
@@ -45,7 +78,8 @@ namespace MovieCollectionValue
         public override void Parse(Response response)
         {
             HtmlNode[] nodes = response.Css("#search  div > div:nth-child(2) > div:nth-child(1) > div > div > div.a-row.a-size-base.a-color-secondary > div > a > span");
-            string price = nodes[0].InnerHtml.ToString();
+            string price = nodes != null ? nodes[0].InnerHtml.ToString() : "0.0";
+
             if(price.Contains("to buy"))
             {
                 double movieValue = 0.0;
@@ -55,8 +89,6 @@ namespace MovieCollectionValue
                 {
                     collectionValue += movieValue;
                 }
-
-                
             }
         }
 
